b4972d03a5cb9b9c06b9414112215660aeeb93c7
Thiago Torres de Souza <thiagomev@gmail.com>
2021-06-02 17:39:56 -0300

    pagina de erro adicionada e modificada

--- src/app/validate-cep/validate-cep.component.ts
+++ src/app/validate-cep/validate-cep.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ValidateCepComponent implements OnInit {
 
+  public isCollapsed = false;
+
   constructor() { }
 
   ngOnInit() {
